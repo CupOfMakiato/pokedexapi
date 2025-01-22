8a8b47621ebc192c550a9d0454ded276024ce353
Makiato <nguyenlmse171333@fpt.edu.vn>
2025-01-22 13:12:01 +0700

    try 7

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedexapi/', 
-  build: {
-    outDir: 'dist', 
-  },
-});
+  plugins: [react()],
+    base: '/pokedexapi/', 
+    build: {
+      outDir: 'dist', 
+    },
+  });
